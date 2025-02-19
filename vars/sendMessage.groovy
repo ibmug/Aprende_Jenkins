b5ed647b8d7f73e3bf7c1f5e38f31cdefc94b5fd
Rodrigo Sanchez <rosv58@gmail.com>
2025-02-19 11:55:15 -0600

    Mmmm

--- vars/sendMessage.groovy
+++ vars/sendMessage.groovy
@@ -2,7 +2,7 @@ def call(Map config){
     node{
         echo ${config.message}
         sh '''
-        echo "Hello, World, this is a groovy file by PirataGomo!" > /tmp/hello.txt
+        echo "Hello, World, this is a groovy file by PirataGomo!"
         git version
         java -version
         mvn -version
