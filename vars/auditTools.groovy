ae36ae5016e35dafd4ae5f493c62c520a934a113
Rodrigo Sanchez <rosv58@gmail.com>
2025-02-19 11:46:57 -0600

    Agregando mas detalles en el hello world

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,7 +1,7 @@
 def call(){
     node{
         sh '''
-        echo "Hello, World!" > /tmp/hello.txt
+        echo "Hello, World, this is a groovy file by PirataGomo!" > /tmp/hello.txt
         git version
         java -version
         mvn -version
